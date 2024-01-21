5dd21a41e76cef4e8300fa65669e421a4fb6005a
Aleix L <thelag55@gmail.com>
2024-01-21 21:51:55 +0100

    error when cp sudoers file

--- config.sh
+++ config.sh
@@ -11,6 +11,7 @@ function setTimeZone() {
 }
 
 function setUpRoot() {
+   echo "Choose the password for the user root"
    passwd
    while [ $? -ne 0 ]; do
       passwd
@@ -64,7 +65,7 @@ function installAndSetUpSudo() {
    InstallPackage "sudo"
    #We'll let a .aui file in case we ever need to back up
    if [[ ! -f /etc/sudoers.aui ]]; then
-      cp -v "/etc/sudoers /etc/sudoers.aui"
+      cp -v "/etc/sudoers" "/etc/sudoers.aui"
       ## Uncomment to allow members of group wheel to execute any command
       sed -i '/%wheel ALL=(ALL) ALL/s/^#//' "/etc/sudoers"
    fi
