878f70f3aebbc6299beeb705c0ed5bbb0d1d5fa9
Aleix L <thelag55@gmail.com>
2024-02-04 14:56:13 +0100

    test gpt

--- userConfig.sh
+++ userConfig.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 
 function InstallPackage() {
-   sudo pacman -S --noconfirm --needed "$1"
+   echo "$password" | sudo -S pacman -S --noconfirm --needed "$1"
 }
 
 function installAUR() {
-   mkdir -p ~/Desktop/repos
+   InstallPackage git
+   read -s -p "Enter sudo password: " password
+   echo "$password" | sudo -S mkdir -p ~/Desktop/repos
    cd ~/Desktop/repos
    git clone https://aur.archlinux.org/paru-bin.git
    cd paru-bin
-   sudo makepkg -si --noconfirm
+   echo "$password" | sudo -S makepkg -si --noconfirm
 }
 
 function installBlackArch() {
-   mkdir -p ~/Desktop/repos/BlackArch
+   InstallPackage curl
+   read -s -p "Enter sudo password: " password
+   echo "$password" | sudo -S mkdir -p ~/Desktop/repos/BlackArch
    cd ~/Desktop/repos/BlackArch
    curl -O https://blackarch.org/strap.sh
    chmod +x strap.sh
-   sudo ./strap.sh
+   echo "$password" | sudo -S ./strap.sh
    InstallPackage blackarch
 }
 
 function setUpHyperland() {
    mkdir -p ~/Downloads
    cd ~/Downloads
+   read -p "Press Enter to continue with Hyperland setup..."
    git clone https://gitlab.com/stephan-raabe/dotfiles.git
    cd dotfiles
+   read -p "Press Enter to start the installation..."
    ./install.sh
 }
 
 function main() {
+    read -s -p "Enter sudo password: " password
     InstallPackage git  # Install git at the beginning
     installAUR  
     installBlackArch
