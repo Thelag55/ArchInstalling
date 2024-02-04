7ae007c11a91852201ce8c617fed4160997e8222
Aleix L <thelag55@gmail.com>
2024-02-04 14:40:01 +0100

    new try

--- userConfig.sh
+++ userConfig.sh
@@ -5,7 +5,6 @@ function InstallPackage() {
 }
 
 function installAUR() {
-   InstallPackage git
    mkdir -p ~/Desktop/repos
    cd ~/Desktop/repos
    git clone https://aur.archlinux.org/paru-bin.git
@@ -18,23 +17,20 @@ function installBlackArch() {
    cd ~/Desktop/repos/BlackArch
    curl -O https://blackarch.org/strap.sh
    chmod +x strap.sh
-   sudo -S ./strap.sh
+   sudo ./strap.sh
    InstallPackage blackarch
 }
 
 function setUpHyperland() {
-   mkdir ~/Downloads
-   # 1.) Change into your Downloads folder
+   mkdir -p ~/Downloads
    cd ~/Downloads
-   # 2.) Clone the dotfiles repository into the Downloads folder
    git clone https://gitlab.com/stephan-raabe/dotfiles.git
-   # 3.) Change into the dotfiles folder
    cd dotfiles
-   # 4.) Start the installation
    ./install.sh
 }
 
 function main() {
+    InstallPackage git  # Install git at the beginning
     installAUR  
     installBlackArch
     setUpHyperland
