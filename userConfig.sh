385055b95dc15f7059bdbf70022ffa81ecc0b160
Aleix L <thelag55@gmail.com>
2024-02-08 23:39:48 +0100

    test tmux error hardcoding values

--- userConfig.sh
+++ userConfig.sh
@@ -23,25 +23,25 @@ function setUpHyperland() {
    session="HyperLand"
    install_script="installer.sh"
 
-   tmux new-session -d -s "$session"
+   tmux new-session -d -s "HyperLand"
 
    window=0
-   tmux rename-window -t "$session:$window" "$session"
-   tmux send-keys -t "$session:$window" "./$install_script; echo 'The script is over'; exit" C-m
+   tmux rename-window -t "HyperLand:0" "HyperLand"
+   tmux send-keys -t "HyperLand:0" "./installer.sh; echo 'The script is over'; exit" C-m
 
-   tmux attach-session -t $session
+   tmux attach-session -t "HyperLand"
    # Wait for the installation script to complete (you may adjust the sleep duration)
    sleep 5
 
    # Check if the installation script has completed successfully
-   if tmux wait-for -S "$session:complete"; then
+   if tmux wait-for -S "HyperLand:complete"; then
       echo "Installation completed successfully."
    else
       echo "Installation failed."
    fi
 
    # Kill the tmux session
-   tmux kill-session -t "$session"
+   tmux kill-session -t "HyperLand"
    read -p "After HyperlandSetUp" test
 
 }
