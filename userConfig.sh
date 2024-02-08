44fd11633679a05d9981a9e949334e4fa01f3cbc
Aleix L <thelag55@gmail.com>
2024-02-08 16:21:34 +0100

    installAUR funciona

--- userConfig.sh
+++ userConfig.sh
@@ -8,7 +8,7 @@ function installAUR() {
    makepkg -s  # This will install dependencies
    makepkg -f    # This will build the package
    read -p "Before install package Aur" test
-   package="$(ls | grep paru-bin | grep -v debug | head -n 1)"
+   package="$(ls | grep paru-bin | grep -v debug | head -n 1)" 
    sudo pacman -U "$package"
    paru  # Replace package_name with the actual package name generated
    cd ..
